45a681cf56bbc83b82b669420910455f4aef9faa
Fabrice Devaux <fabrice.devaux@gmail.com>
2026-05-25 08:23:40 +0200

    stringcalc: initial version

---
+++ stringcalc.py
@@ -0,0 +1,33 @@
+"""String Calculator Kata"""
+
+
+def add(expr: str) -> int:
+    expr = _normalize_separators(expr)
+
+    if not expr:
+        return 0
+
+    integers = _extract_integers(expr)
+    _validate_integers(integers)
+
+    return sum(_extract_integers(expr))
+
+
+def _normalize_separators(expr: str) -> str:
+    if expr.startswith("//"):
+        custom_separator, expr = expr.split("\n", maxsplit=1)
+        custom_separator = custom_separator[2:]
+        expr = expr.replace(custom_separator, ",")
+
+    return expr.replace("\n", ",")
+
+
+def _extract_integers(expr: str) -> list[int]:
+    return [int(i) for i in expr.split(",")]
+
+
+def _validate_integers(integers: list[int]) -> None:
+    # negatives = [str(i) for i in integers if i < 0]
+    # if negatives:
+    #     raise ValueError(f"negatives not allowed: {' '.join(negatives)}")
+    return
---
+++ test_stringcalc.py
@@ -0,0 +1,47 @@
+import pytest
+
+import stringcalc
+
+
+def test_add_empty_string():
+    assert stringcalc.add("") == 0
+
+
+def test_add_zero():
+    assert stringcalc.add("0") == 0
+
+
+def test_add_one_int():
+    assert stringcalc.add("4") == 4
+    assert stringcalc.add("45") == 45
+    assert stringcalc.add("124") == 124
+
+
+def test_add_two_ints():
+    assert stringcalc.add("1,2") == 3
+    assert stringcalc.add("0,45,5") == 50
+    assert stringcalc.add("672,89") == 761
+
+
+def test_add_many_ints():
+    assert stringcalc.add("1,2,3,4,5,6,7,8,9") == 45
+
+
+def test_add_with_newline_sep():
+    assert stringcalc.add("1\n2,3") == 6
+
+
+def test_add_with_custom_sep():
+    assert stringcalc.add("//;\n1;2") == 3
+    assert stringcalc.add("//,\n1,2") == 3
+
+
+def test_add_mixed_separators():
+    assert stringcalc.add("//;\n1\n2,3;4") == 10
+
+
+# def test_add_reject_negatives():
+#     with pytest.raises(ValueError) as exc:
+#         stringcalc.add("1,-2,-3")
+
+#     assert str(exc.value) == "negatives not allowed: -2 -3"
